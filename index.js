aa9a97bf0629b6d7409dc500868b56b8af6b18f4
gizemcdeniz <gizemc.deniz@gmail.com>
2020-10-25 17:02:20 +0300

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+const divide = () => {
+    return 2000 / 100;
+}
+
+divide();
+
+
+
+
+const square = () => {
+    let x = 2;
+    return x * x;
+}
+
+square();
+
+
+const add = () => {
+    let a = 3;
+    let b = 4;
+    return a + b
+}
+
+add();
